e10aa2c5846e5bf85526df2a97327d86e14969a5
Kishoyan-Brian <brianmwangiat033@gmail.com>
2026-06-04 09:51:11 +0300

    DELETE: added a delete end point

--- api/Controllers/StocksController.cs
+++ api/Controllers/StocksController.cs
@@ -74,6 +74,20 @@ namespace api.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        [HttpDelete("{id}")]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();   
+            }
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 
 }
