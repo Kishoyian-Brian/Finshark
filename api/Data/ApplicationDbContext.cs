2308371d376ed8bd3a792dc10e219996007fac15
Kishoyan-Brian <brianmwangiat033@gmail.com>
2026-06-10 09:13:33 +0300

    feat:Authenntication using JWTbearer

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole, string>
     {
-        public ApplicationDbContext(DbContextOptions  dbContextOptions) :base(dbContextOptions)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
         }
+
         public DbSet<Stocks> Stocks { get; set; }
         public DbSet<Comments> Comments { get; set; }
     }
--- api/Program.cs
+++ api/Program.cs
@@ -3,6 +3,10 @@ using api.Data;
 using DotNetEnv;
 using api.interfaces;
 using api.Repository;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 Env.TraversePath().Load();
 
@@ -16,7 +20,7 @@ var connectionString =
     $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
 
 
-builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -29,6 +33,40 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(connectionString);
 });
+builder.Services.AddIdentity<AppUser,IdentityRole>(options =>
+{
+    options.Password.RequireDigit=true;
+    options.Password.RequireLowercase=true;
+    options.Password.RequireUppercase=true;
+    options.Password.RequireNonAlphanumeric=true;
+    options.Password.RequiredLength=12;
+    })
+
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = 
+    options.DefaultChallengeScheme=
+    options.DefaultForbidScheme=
+    options.DefaultScheme=
+    options.DefaultSignInScheme=
+    options.DefaultSignOutScheme=JwtBearerDefaults.AuthenticationScheme;
+    
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer=true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience=true,
+        ValidAudience=builder.Configuration["JWT:Audience"],
+        ValidateIssuerSigningKey= true,
+        IssuerSigningKey=new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+        )
+    };
+});   
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<IcommentRepository, CommentRepository>();
@@ -40,7 +78,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
